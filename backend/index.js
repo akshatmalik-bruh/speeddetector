dd370a5a0d76e74747adad9a5b94340e4dd03d0c
Akshat malik <akshatf8lmalik@gmail.com>
2026-02-21 15:47:23 +0530

    url change to deploy

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.vercel.app"
+      "https://speeddetector-iota.vercel.app"
     ],
     credentials: true
   })
